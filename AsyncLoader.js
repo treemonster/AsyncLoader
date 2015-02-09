1a66a492a7e8c806993cbb7209322fecbd78ba7a
chen wen'gao <1696292264@qq.com>
2015-02-09 15:43:31 +0800

    Create AsyncLoader.js

---
+++ AsyncLoader.js
@@ -0,0 +1,115 @@
+window.AsyncLoader={};
+var define;
+(function(loader,win){
+	var loads=[],loaded={},module={},wait={},wi=0,callbackwait={};
+	win.module=module;
+	function is(a,b){return a!==undefined && a.constructor===b;}
+	function format(request,refer){
+		request=refer+request;
+		var re=[];
+		for(var i=0,req=request.split('/');i<req.length;i++)
+			if(req[i]==='.')continue;
+			else if(req[i]==='..')re.pop();
+			else re.push(req[i]);
+		return re.join('/');
+	}
+	function loadScript(src){
+		var script=document.createElement('script');
+		var head=document.getElementsByTagName('head')[0];
+		script.onerror=function(){return;try{head.removeChild(this);}catch(e){}};
+		var load=function(){
+			if(this.loaded==true)return;
+			this.loaded=true;
+			loads.push(this);
+			this.onerror();
+		};
+		if(script.onreadystatechange===null)
+			script.onreadystatechange=function(){if(/loaded|complete/.test(this.readyState))load.call(this);};
+		else script.onload=load;
+		script.type='text/javascript';
+		script.async="async";script.defer="defer";
+		script.src=(script.moduleName=src.replace(/\.js(\?.*)*$/i,''))+'.js';
+		if(head)head.insertBefore(script,head.firstChild);
+	}
+	function test(requires,refer){
+		var ready=[];
+		for(var i=0;i<requires.length;i++){
+			if(refer!==undefined)requires[i]=format(requires[i],refer);
+			if(module[requires[i]]){
+				if(ready!==null)ready.push(requires[i]);
+			}else{
+				loadScript(requires[i]);
+				ready=null;
+			}
+		}
+		return [requires,ready];
+	}
+	define=function(){
+		var a=arguments;
+		if(this.ready!=true)return setTimeout(function(){
+			define.apply({ready:true,node:loads.shift()},a);
+		},0);
+		if(this.moduleName===undefined){
+			this.moduleName=this.node.moduleName;
+			this.path=this.moduleName.replace(/[^\/]+$/,'');
+			delete this.node;
+		}
+		var id,dependencies,factory,l=a.length,node=this,tested=node.tested||a[3];
+
+		switch(l-(l>3)){
+			case 3:
+				id=a[0],dependencies=a[1],factory=a[2];
+				break;
+			case 2:
+				if(is(a[0],String))id=a[0],dependencies=[],factory=a[1];
+				else id=undefined,dependencies=a[0],factory=a[1];
+				break;
+			case 1:
+				id=undefined,dependencies=[],factory=a[0];
+				break;
+		}
+		if(tested===undefined){
+			if(id)id=format(node.path+id,'./');
+			else id=format(node.moduleName,'./');
+			tested=test(dependencies,node.path);
+			if(loaded[id])return;
+			loaded[id]=true;
+			node.moduleName=id;
+		}else if(tested[1]===null){
+			wait[wi++]=[id,dependencies,factory,node];
+			return;
+		}
+		var w,m={exports:(module[id]={})};
+		var getRequire=function(moduleName,callback){
+			if(is(moduleName,String))moduleName=[moduleName];
+			var t=test(moduleName,node.path);
+			if(callback){
+				if(t[1]!==null)callback.apply(node,t[1]);
+				else callbackwait[wi++]=[callback,t[0],node];
+			}
+		};
+		getRequire.toUrl=function(str){
+			return format(str,node.path);
+		};
+		if(is(factory,Object))module[id]=factory;
+		else factory(getRequire,m.exports,m);
+
+		for(var i in callbackwait){
+			var t=test(callbackwait[i][1]);
+			if(t[1]!==null){
+				var cb=callbackwait[i].slice(0);
+				delete callbackwait[i];
+				cb[0] && cb[0].apply(cb[2],t[1]);
+			}
+		}
+
+		for(var i in wait){
+			if(test(wait[i][1])[1]===null)return;
+			w=wait[i].slice(0);
+			delete wait[i];
+			define.apply(w[3],w);
+		}
+	};
+	define.amd={};
+	loader.use=function(src){loadScript(src)};
+})(AsyncLoader,window);
